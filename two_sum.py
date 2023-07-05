5361acf52e73726f2dedde3d391aefb351ef7d87
smithaagarwal <smitha.kotian@gmail.com>
2023-07-05 11:54:34 +0100

    Added solution for Leet Code, Two sums problem

--- two_sum.py
+++ two_sum.py
@@ -15,6 +15,8 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 """""
+
+
 class Solution(object):
     def twoSum(self, nums, target):
         result = []
@@ -30,6 +32,7 @@ class Solution(object):
                         result.append(index1)
                         result.append(index2)
                         return result
+
 
 nums = [2, 7, 11, 15]
 target = 26
