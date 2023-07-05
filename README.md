4eb963e97cbd2a67fa204629f840eabe4a7f6f5a
smithaagarwal <smitha.kotian@gmail.com>
2023-07-05 16:00:16 +0100

    Added solution for Leet Code, Palindrome problem without converting the integer to string

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #This is a project made to learn python and try different features
 #4th July 23, setup coding and version control env, variables, datatypes, working with string numbers, input, math library
+#5th July 23, Leet Code  - easy - Two Sums, Palindrome
--- palindrome_number.py
+++ palindrome_number.py
@@ -1,3 +1,17 @@
+"""""
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Could you solve it without converting the integer to a string?
+"""""
 from math import *
 class Solution:
     def isPalindrome(self, x: int) -> bool:
