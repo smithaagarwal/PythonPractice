2b7fb78babb0bb65e1769625be468e4e44af3325
smithaagarwal <smitha.kotian@gmail.com>
2023-07-05 12:07:47 +0100

    Added solution for Leet Code, Two sums problem

--- two_sum.py
+++ two_sum.py
@@ -15,10 +15,11 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 """""
+from typing import List
 
 
 class Solution(object):
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         result = []
         """
         :type nums: List[int]
